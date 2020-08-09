5bc46dbd3d94453ab548fe248c68058f5b2626fd
Chinmay Kabi <chinmay.kabi@live.com>
2020-08-10 02:00:34 +0530

    dummy GraphQL api tested

--- extension/background.js
+++ extension/background.js
@@ -1,6 +1,45 @@
+const safeIcon = 'icons/shield.png';
+const riskIcon = 'icons/caution.png';
+const stopIcon = 'icons/stop.png';
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if (request.greeting === "GetURL") {
         var tabURL = request.sentData;
-        alert(tabURL);
+        //alert(tabURL);
+        getData();
+        let select = getRandomInt(0, 2);
+        if (select == 0)
+            chrome.browserAction.setIcon({ path: safeIcon });
+        else if (select == 1)
+            chrome.browserAction.setIcon({ path: riskIcon });
+        else chrome.browserAction.setIcon({ path: stopIcon });
+
     }
-})
+});
+
+function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getData() {
+    fetch('https://metaphysics-staging.artsy.net/', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ query: `{
+                popular_artists {
+                  artists {
+                    name
+                  }
+                }
+              }` }),
+        })
+        .then(res => res.json())
+        .then(data => alert(data.extensions.requestID));
+
+}
+
+function parsemessage(res) {
+    //const obj = JSON.parse(res.json());
+    alert(res.status);
+}
