71c69c337437c8a7bcf634024bfa1b17dd409910
DesignrKnight <notify.abelmathew@gmail.com>
2020-08-10 03:25:19 +0530

    REST INit

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema/schema');
+const {getMetadata} = require('page-metadata-parser');
+const domino = require('domino');
+const fetch = require("node-fetch");
+var stringSimilarity = require('string-similarity');
+const tldjs = require('tldjs');
+require('dotenv').config()
 
 const app = express();
 
@@ -10,6 +16,27 @@ app.use('/graphql', graphqlHTTP({
     graphiql: true
 }));
 
-app.listen(4000, () => {
-    console.log('now listening for requests on port 4000');
+
+const searchURLbyDomain = async(url) => {
+    const response = await fetch(url).catch(err=>console.log(err));;
+    const html = await response.text().catch(err=>console.log(err));;
+    const doc = domino.createWindow(html).document;
+    const metadata = getMetadata(doc, url);
+    console.log(metadata)
+    fetchedURL=await fetch(`https://authhack.cognitiveservices.azure.com/bing/v7.0/entities?mkt=en-US&q=${tldjs.parse(metadata.url).hostname}`, { headers: { 'Ocp-Apim-Subscription-Key': process.env.OcpApimSubscriptionKey } })
+    .then(res => res.json())
+    .then(json => {
+        return json.entities.value[0].url?json.entities.value[0].url:undefined
+    })
+    .catch(err=>console.log(err));
+    var similarity = stringSimilarity.compareTwoStrings(fetchedURL, metadata.url);
+    return similarity
+}
+app.use(express.static('./screenshot_cluster')); //Serves resources from public folder
+app.get('/urlstatus',(req,res)=>{
+    searchURLbyDomain(req.query.url).then(param=>res.send(`Got GET with similarity ${param}`))
+})
+
+app.listen(80, () => {
+    console.log('now listening for requests on port 80');
 });
