7d762fa4350a1a9e840cc7cbc4466843a5639bce
Chinmay Kabi <chinmay.kabi@live.com>
2020-08-10 10:32:14 +0530

    Minor Changes in popup.js

--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,18 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('checkPage');
-    checkPageButton.addEventListener('click', function() {
+    // var checkPageButton = document.getElementById('checkPage');
+    // checkPageButton.addEventListener('click', function() {
+    //     chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
+    //         chrome.runtime.sendMessage({ greeting: "GetURL", sentData: String(tabs[0].url) });
+
+    //     });
+    // }, false);
+
+    var changeColorButton = document.getElementById('changeColor');
+    changeColorButton.addEventListener('click', function() {
         chrome.tabs.query({ currentWindow: true, active: true }, function(tabs) {
             chrome.runtime.sendMessage({ greeting: "GetURL", sentData: String(tabs[0].url) });
 
         });
-    }, false);
-
-    var changeColorButton = document.getElementById('changeColor');
-    changeColorButton.addEventListener('click', function() {
-
         chrome.runtime.sendMessage({ greeting: "changeBkgCol" });
     });
 
