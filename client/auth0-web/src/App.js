6a4ae7c00ba46ed3cf38bddd6f329abf2d1c4570
DesignrKnight <notify.abelmathew@gmail.com>
2020-08-10 18:18:16 +0530

    DO Push

--- client/auth0-web/src/App.js
+++ client/auth0-web/src/App.js
@@ -1,16 +1,47 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import { useForm } from "react-hook-form";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+const IMAGES =[]
+
+
+
+export default function App() {
+  const { user, isAuthenticated } = useAuth0();
+  const { register, handleSubmit, watch, errors } = useForm();
+  const [status,setstatus]=React.useState(false)
+  const renderImage=(imageUrl) =>{
+    return (
+      <div>
+        <img src={imageUrl} width="70%" />
+      </div>
+    );
+  }
+  const onSubmit =async data => {
+    if(isAuthenticated){
+      console.log(data);  
+      await fetch(`http://159.89.172.222/urlstatus?url=${data.exampleRequired}`)
+      .then(response => response.json())
+      .then(data=>{
+        for(let i=0;i<5;i++){
+          IMAGES.push(`http://159.89.172.222/${data.filename}_desktop${i}.png`)
+        }
+        setstatus(true)
+      })
+    }
+    else
+    {
+      alert("Login is Required")
+    }
+  }
   return (
-    <div class="wrap">
-   <div class="search">
-      <input type="text" class="searchTerm" placeholder="Enter Valid URL"/>
-      <button type="submit" class="searchButton">
-        <i class="fa fa-search"></i>
-     </button>
-   </div>
-</div>
+    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input name="exampleRequired" ref={register({ required: true })} />
+      {errors.exampleRequired && <span>This field is required</span>}
+      <input type="submit" />
+    </form>
+    {IMAGES.map(imageUrl=>renderImage(imageUrl))}
+    </>
   );
 }
-export default App;
